e8105a535ec6a62e99833b20b558fce398a3db77
sboris <smrnvbrs@gmail.com>
2020-11-21 03:24:18 +0300

    initial, added nodemon, express and morgan

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const { PORT } = require("./utlis/constants");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.listen(process.env.PORT || PORT, (err) => {
+  console.log(`Listening on ${PORT}`);
+  if (err) {
+    console.error(`Server can't start. Error: ${err}`);
+  }
+});
