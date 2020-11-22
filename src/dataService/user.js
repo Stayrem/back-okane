4b4a7343aed037d76dcc4c637c76c827a9b0ccee
sboris <smrnvbrs@gmail.com>
2020-11-22 20:49:01 +0300

    added user data-service and it's creation method

---
+++ src/dataService/user.js
@@ -0,0 +1,21 @@
+class UserService {
+  constructor(database) {
+    const { models } = database;
+    const { User, Income, Cost, Saving, Spending, Saldo } = models;
+    this._database = database;
+    this._models = models;
+  }
+  async create({ email, password }) {
+    const { User } = this._models;
+    try {
+      const newUser = await User.create({
+        email,
+        password,
+      });
+      return `Created: ${newUser}`;
+    } catch (err) {
+      console.log(err);
+      return `Can't create user. ${err}`;
+    }
+  }
+}
