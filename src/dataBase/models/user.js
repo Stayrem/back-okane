ae3f46387348269b0eee87b385651ba0b22a988d
sboris <smrnvbrs@gmail.com>
2020-11-27 01:24:42 +0300

    added cascade action on user delete (just for no reason)

--- src/dataBase/models/user.js
+++ src/dataBase/models/user.js
@@ -43,26 +43,32 @@ const createAssociations = ({ User, Income, Cost, Saving, Spending, Saldo, Refre
   User.hasMany(Income, {
     as: `incomes`,
     foreignKey: `user_id`,
+    onDelete: "CASCADE",
   });
   User.hasMany(Cost, {
     as: `costs`,
     foreignKey: `user_id`,
+    onDelete: "CASCADE",
   });
   User.hasMany(Saving, {
     as: `savings`,
     foreignKey: `user_id`,
+    onDelete: "CASCADE",
   });
   User.hasMany(Spending, {
     as: `spendings`,
     foreignKey: `user_id`,
+    onDelete: "CASCADE",
   });
   User.hasMany(Saldo, {
     as: `saldo`,
     foreignKey: `user_id`,
+    onDelete: "CASCADE",
   });
   User.hasOne(RefreshToken, {
     as: `refreshTokens`,
     foreignKey: `user_id`,
+    onDelete: "CASCADE",
   });
 };
 
